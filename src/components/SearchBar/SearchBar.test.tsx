9647827590eef502bd2c6e9805631af2756404a1
Gabriel Camargo <gcamargo.dev@gmail.com>
2023-10-10 11:31:59 -0300

    fix: fix issue that jest didn't know how to handle some libraries and remove unused libraries to clear storage occupied with unused files

--- src/components/SearchBar/SearchBar.test.tsx
+++ src/components/SearchBar/SearchBar.test.tsx
@@ -1,5 +1,45 @@
-describe('SearchBar component', function () {
-  it('should be equal 4', function () {
-    expect(2 + 2).toBe(4);
+import { render } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SearchBar } from '.';
+import { LocationContext } from '../../contexts/LocationContext';
+
+jest.mock('react-dom', () => {
+  const original = jest.requireActual('react-dom');
+  return {
+    ...original,
+    createPortal: (node: HTMLElement) => node
+  };
+});
+
+describe('SearchBar component', () => {
+  let container: HTMLDivElement;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(container);
+  });
+
+  it('should render SearchBar', () => {
+    const locationContextValue = {
+      selectedState: 'RS',
+      setSelectedState: jest.fn(),
+      selectedCity: 'Porto Alegre',
+      setSelectedCity: jest.fn(),
+    };
+
+    const queryClient = new QueryClient();
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <LocationContext.Provider value={locationContextValue}>
+          <SearchBar />
+        </LocationContext.Provider>
+      </QueryClientProvider>,
+      { container }
+    );
   });
 });
