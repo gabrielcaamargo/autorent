f94ba115d20efcecc369eb26b2799e85a8527ce5
Gabriel Camargo <gcamargo.dev@gmail.com>
2023-10-17 01:33:42 -0300

    style: apply prettier bracket rules

--- src/components/FilterOption/index.tsx
+++ src/components/FilterOption/index.tsx
@@ -1,14 +1,23 @@
-interface FilterOptionProps {
+interface IFilterOptionProps {
   label: string;
   isActive?: boolean;
   icon?: React.ReactNode;
   onClick?: () => void;
 }
 
-export function FilterOption({ label, isActive, icon, onClick }: FilterOptionProps) {
+export function FilterOption({
+  label,
+  isActive,
+  icon,
+  onClick,
+}: IFilterOptionProps) {
   return (
     <button
-      className={`font-bold text-lg px-3 ease-in duration-150 ${isActive ? 'bg-carmesim-500 text-white hover:bg-carmesim-600' : 'bg-gray-100 text-gray-600 hover:bg-gray-300'} rounded flex items-center gap-2`}
+      className={`font-bold text-lg px-3 ease-in duration-150 ${
+        isActive
+          ? 'bg-carmesim-500 text-white hover:bg-carmesim-600'
+          : 'bg-gray-100 text-gray-600 hover:bg-gray-300'
+      } rounded flex items-center gap-2`}
       onClick={onClick}
     >
       {icon}
